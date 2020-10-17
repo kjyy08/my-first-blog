84573da6826a9177884bd02c674cf9266be60b19
kjyy08 <kjyy08@naver.com>
2020-10-18 00:02:12 +0900

    Update settings.py

--- KChat/settings.py
+++ KChat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4o0-n88g)fqdv)86afv%2!w!jhw@k+6@pfiq1j9e^3lr@f^7dp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
